733a988471eae27f60093ed16cf538214ea21cea
Eddie <ed.heymann@gmail.com>
2020-02-16 16:45:31 -0600

    Added 'delete' functionality to the app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
@@ -7,7 +7,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Edwin3385@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 
@@ -21,15 +20,32 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
 
 
+@app.route('/todo/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+
+
+# note: more conventionally, we would write a
+# POST endpoint to /todo for the create endpoint:
+# @app.route('/todo', method=['POST'])
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        todo = Todo(description=description, completed=False)
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
         body['description'] = todo.description
     except:
         error = True
@@ -60,4 +76,4 @@ def set_completed_todo(todo_id):
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', todo=Todo.query.order_by('id').all())
